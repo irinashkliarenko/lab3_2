486387cefafd9e5a715e8f8160850fe7cba2909c
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-05-30 15:35:00 +0200

    PublishableNewsForTests class added

---
+++ src/main/java/edu/iis/mto/staticmock/PublishableNewsForTests.java
@@ -0,0 +1,24 @@
+package edu.iis.mto.staticmock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PublishableNewsForTests extends PublishableNews{
+	
+	private final List<String> publicInfo = new ArrayList<>();
+	private final List<String> subscriptionInfo = new ArrayList<>();
+	
+	
+	@Override
+	public void addPublicInfo(String content) {
+		super.addPublicInfo(content);
+		publicInfo.add(content);
+	}
+	
+	@Override
+	public void addForSubscription(String content, SubsciptionType subscriptionType) {
+		super.addForSubscription(content, subscriptionType);
+		subscriptionInfo.add(content);
+	}
+
+}
