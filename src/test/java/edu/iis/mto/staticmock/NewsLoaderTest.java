3af01e782e94312b54f61e5ce6dfed089e7b2ee0
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-05-30 15:48:05 +0200

    setUp methond implemented

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -1,15 +1,62 @@
 package edu.iis.mto.staticmock;
 
-import static org.junit.Assert.*;
-
-
+import org.junit.Before;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
+import edu.iis.mto.staticmock.reader.NewsReader;
+
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class})
 public class NewsLoaderTest {
+	private IncomingNews incomingNews = new IncomingNews();
+	
+	@Before
+	public void setUp() {
+		IncomingInfo incomingInfoPublic = new IncomingInfo("public content", SubsciptionType.NONE);
+		IncomingInfo incomingInfoForSubscribers = new IncomingInfo("content for subsribers A", SubsciptionType.A);
+		
+		
+        mockStatic(ConfigurationLoader.class);
+        ConfigurationLoader mockConfigurationLoader = mock(ConfigurationLoader.class);
+        when(ConfigurationLoader.getInstance()).thenReturn(mockConfigurationLoader);
 
+        Configuration configuration = new Configuration();
+        String readerType = "File";
+        Whitebox.setInternalState(configuration, "readerType", readerType);
+        when(mockConfigurationLoader.loadConfiguration()).thenReturn(configuration);
+
+        mockStatic(PublishableNews.class);
+        when(PublishableNews.create()).thenReturn(new PublishableNewsForTests());
+
+        incomingNews = new IncomingNews();
+        incomingNews.add(incomingInfoPublic);
+        incomingNews.add(incomingInfoForSubscribers);
+
+        NewsReader newsReader = new NewsReader() {
+            @Override
+            public IncomingNews read() {
+                return incomingNews;
+            }
+        };
+        
+        mockStatic(NewsReaderFactory.class);
+        when(NewsReaderFactory.getReader(readerType)).thenReturn(newsReader);
+
+        NewsLoader newsLoader = new NewsLoader();		
+	}
+	
 	@Test
 	public void test() {
-		
+
 	}
 
 }
